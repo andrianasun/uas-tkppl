3b436893200b9d01025fc8e678caf5e5ea1fe1f3
Andriana Sunjaya <andrianasun828@gmail.com>
2023-01-25 23:01:45 +0700

    Menyelesaikan halaman WelcomeScreen

---
+++ app/src/main/java/com/example/tkppl_uas/WelcomeScreenActivity.kt
@@ -0,0 +1,22 @@
+package com.example.tkppl_uas
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class WelcomeScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome_screen)
+
+        fun onSignInClick(view: View) {
+            val intent = Intent(this, SigninActivity::class.java)
+            startActivity(intent)
+        }
+        fun onSignUpClick(view: View) {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
