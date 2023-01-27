e288442d34cf28bd63aa58c7c5ed4ac53f90d73f
ipipne <ipipne0302@gmail.com>
2023-01-27 10:01:41 +0700

    Menyelesaikan Produck Detail dan Nego

---
+++ app/src/main/java/com/example/tkppl_uas/Konstanta.kt
@@ -0,0 +1,18 @@
+package com.example.tkppl_uas
+
+//Nego Count
+const val EXTRA_NEGO_COUNT = "EXTRA_NEGO_COUNT"
+
+//Edit Profile
+const val EXTRA_EDIT_PROFILE = "EXTRA_EDIT_PROFILE"
+const val EXTRA_REQUEST_EDITPROFILE_CODE = 100
+const val EXTRA_RESULT_EDITPROFILE_CODE = 101
+const val EXTRA_CANCEL_EDITPROFILE_CODE = -101
+
+const val JOB_ID = 3213
+const val EXTRA_TIME = "EXTRA_TIME"
+const val JOB_ID_PROGRESS = 12345
+const val ACTION_PROGRESS = "Action_Progress"
+const val EXTRA_PERSEN = "persen"
+const val EXTRA_FINISH = "finish"
+
