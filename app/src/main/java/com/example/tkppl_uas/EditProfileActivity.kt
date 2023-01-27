d02ff7b3af8e0748e1d517f66885c53f2cff9d8f
Andriana Sunjaya <andrianasun828@gmail.com>
2023-01-27 19:09:03 +0700

    menyelesaikan fitur editprofil

--- app/src/main/java/com/example/tkppl_uas/EditProfileActivity.kt
+++ app/src/main/java/com/example/tkppl_uas/EditProfileActivity.kt
@@ -1,11 +1,35 @@
 package com.example.tkppl_uas
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tkppl_uas.classes.ClassEditProfile
+import kotlinx.android.synthetic.main.activity_edit_profile.*
 
 class EditProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
     }
+
+    fun onBtnSaveProfileClicked(view: View) {
+        var username = Username.text.toString()
+        var email = Email.text.toString()
+        var password = Password.text.toString()
+        var phone = Phone.text.toString()
+        var editProfile = ClassEditProfile (username, email, password, phone)
+        var intentdata = Intent()
+        if(username.isEmpty() || email.isEmpty() || password.isEmpty() || phone.isEmpty()){
+            setResult(EXTRA_CANCEL_EDITPROFILE_CODE, intentdata)
+        }
+        else{
+            intentdata.putExtra(EXTRA_EDIT_PROFILE, editProfile)
+            setResult(EXTRA_RESULT_EDITPROFILE_CODE, intentdata)
+        }
+//        Toast.makeText(this, "Save profile success", Toast.LENGTH_SHORT).show()
+        finish()
+    }
+
+
 }
